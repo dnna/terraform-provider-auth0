5739acdbdd0ca18a853452dab2208e832d252ae4
Vishal Kotcherlakota <vishal.kotcherlakota@quanticmind.com>
2018-04-30 09:14:50 -0700

    Updating to use the client_id/client_secret flow

--- provider.go
+++ provider.go
@@ -1,9 +1,13 @@
 package main
 
 import (
-	"log"
+	"log";
 
-	"github.com/hashicorp/terraform/helper/schema"
+	"github.com/hashicorp/terraform/helper/schema";
+	"io/ioutil";
+	"strings";
+	"net/http";
+	"encoding/json";
 )
 
 func Provider() *schema.Provider {
@@ -14,10 +18,15 @@ func Provider() *schema.Provider {
 				Required:    true,
 				DefaultFunc: schema.EnvDefaultFunc("MANAGEMENT_API_DOMAIN", nil),
 			},
-			"access_token": &schema.Schema{
+			"client_id": &schema.Schema{
+				Type:		 schema.TypeString,
+				Required:	 true,
+				DefaultFunc: schema.EnvDefaultFunc("MANAGEMENT_API_CLIENT_ID", nil),
+			},
+			"client_secret": &schema.Schema{
 				Type:        schema.TypeString,
 				Required:    true,
-				DefaultFunc: schema.EnvDefaultFunc("MANAGEMENT_API_ACCESS_TOKEN", nil),
+				DefaultFunc: schema.EnvDefaultFunc("MANAGEMENT_API_CLIENT_SECRET", nil),
 			},
 		},
 
@@ -31,11 +40,45 @@ func Provider() *schema.Provider {
 }
 
 type Config struct {
-	domain      string
-	accessToken string
+	domain       string
+	accessToken  string
+}
+
+type Auth0Token struct {
+	AccessToken string `json:"access_token"`
+	ExpiresIn int `json:"expires_in"`
+	Scope string `json:"scope"`
+	TokenType string `json:"token_type"`
 }
 
 func providerConfigure(d *schema.ResourceData) (interface{}, error) {
 	log.Println("[INFO] Initializing Auth0 client")
-	return Config{domain: d.Get("domain").(string), accessToken: d.Get("access_token").(string)}, nil
+	domain := d.Get("domain").(string)
+	client_secret := d.Get("client_secret").(string)
+	client_id := d.Get("client_id").(string)
+	
+	url := "https://" + domain + "/oauth/token"
+	
+	// payload := strings.NewReader("{\"grant_type\":\"client_credentials\",\"client_id\": \"rOlwtvTBtmNER5AsdTgx1Xjd3gnRHapW\",\"client_secret\": \"YOUR_CLIENT_SECRET\",\"audience\": \"https://quanticmind.auth0.com/api/v2/\"}")
+	payload := strings.NewReader(`{ "grant_type": "client_credentials", ` +
+		`"client_id": "` + client_id +  `", ` +
+		`"client_secret": "` + client_secret + `", ` + 
+		`"audience": "https://` + domain + `/api/v2"}`)
+
+	req, _ := http.NewRequest("POST", url, payload)
+	req.Header.Add("content-type", "application/json")
+	req.Header.Add("content-type", "application/json")
+
+	res, _ := http.DefaultClient.Do(req)
+
+	defer res.Body.Close()
+	body, _ := ioutil.ReadAll(res.Body)
+
+	var auth0Token Auth0Token
+	err := json.Unmarshal(body, &auth0Token)
+	if err != nil {
+		log.Println("[ERROR] Failed to Unmarshal Auth0 Response")
+	}
+
+	return Config{ domain: domain, accessToken: auth0Token.AccessToken}, nil
 }
