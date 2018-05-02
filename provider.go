99780b5a356a388e101ca93b0257c1ace63359e0
Vishal Kotcherlakota <vishal.kotcherlakota@quanticmind.com>
2018-05-02 09:28:01 -0700

    tests pass

--- provider.go
+++ provider.go
@@ -8,6 +8,8 @@ import (
 	"strings";
 	"net/http";
 	"encoding/json";
+	"errors"
+	"strconv"
 )
 
 func Provider() *schema.Provider {
@@ -54,30 +56,52 @@ type Auth0Token struct {
 func providerConfigure(d *schema.ResourceData) (interface{}, error) {
 	log.Println("[INFO] Initializing Auth0 client")
 	domain := d.Get("domain").(string)
-	client_secret := d.Get("client_secret").(string)
-	client_id := d.Get("client_id").(string)
+	clientSecret := d.Get("client_secret").(string)
+	clientId := d.Get("client_id").(string)
 	
+	return providerConfigureRaw(http.DefaultClient, domain, clientId, clientSecret)
+}
+
+func providerConfigureRaw(client *http.Client, domain string, clientId string, clientSecret string) (interface{}, error){
 	url := "https://" + domain + "/oauth/token"
 	
 	payload := strings.NewReader(`{ "grant_type": "client_credentials", ` +
-		`"client_id": "` + client_id +  `", ` +
-		`"client_secret": "` + client_secret + `", ` + 
-		`"audience": "https://` + domain + `/api/v2"}`)
+		`"client_id": "` + clientId +  `", ` +
+		`"client_secret": "` + clientSecret + `", ` + 
+		`"audience": "https://` + domain + `/api/v2/"}`)
 
 	req, _ := http.NewRequest("POST", url, payload)
 	req.Header.Add("content-type", "application/json")
-	req.Header.Add("content-type", "application/json")
 
-	res, _ := http.DefaultClient.Do(req)
+	res, postErr := client.Do(req)
+	if postErr != nil {
+		log.Printf("[ERROR] Failed to contact Auth0 API: %s", postErr)
+		return Config{}, postErr
+	}
 
 	defer res.Body.Close()
-	body, _ := ioutil.ReadAll(res.Body)
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		log.Printf("[ERROR] Failed to read Response: %s", err)
+		return Config{}, err
+	}
+
+	if res.StatusCode < 200 || res.StatusCode >= 300 {
+		log.Printf("[ERROR] Received HTTP Response Code %d from %s: %s", res.StatusCode, url, body)
+		errorText := url + ` responded with code ` + strconv.Itoa(res.StatusCode) + `: ` + string(body)
+		return Config{}, errors.New(errorText)
+	}
+
+	log.Printf("[DEBUG] Response was: %s", body)
 
 	var auth0Token Auth0Token
-	err := json.Unmarshal(body, &auth0Token)
-	if err != nil {
-		log.Println("[ERROR] Failed to Unmarshal Auth0 Response")
+	unmarshalErr := json.Unmarshal(body, &auth0Token)
+	if unmarshalErr != nil {
+		log.Printf("[ERROR] Failed to Unmarshal Auth0 Response: %s", unmarshalErr)
+		return Config{}, unmarshalErr
 	}
+
+	log.Printf("[DEBUG] Domain is %s, token is %s", domain, auth0Token.AccessToken)
 
 	return Config{ domain: domain, accessToken: auth0Token.AccessToken}, nil
 }
---
+++ provider_test.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+    "net/http/httptest"
+    "net/http"
+    "testing"
+    "github.com/stretchr/testify/assert"
+    "encoding/json"
+    "io/ioutil"
+    "fmt"
+)
+
+type ClientRequest struct {
+    GrantType string `json:"grant_type"`
+    ClientId string `json:"client_id"`
+    ClientSecret string `json:"client_secret"`
+    Audience string `json:"audience"`
+}
+
+// func TestProvider(t *testing.T) {
+//     if err := Provider().(*schema.Provider).InternalValidate(); err != nil {
+//         t.Fatalf("err: %s", err)
+//     }
+// }
+
+func TestProviderConfigRawSad(t *testing.T) {
+    assert := assert.New(t)
+    clientSecret := "cauliflower"
+    clientId := "joebang"   
+
+    testServer := httptest.NewTLSServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+        w.WriteHeader(400)
+        w.Header().Set("content-type", "application/json")
+        fmt.Fprintf(w, `{"error":"access_denied","error_description":"Service not enabled within domain: https://dshbreak.auth0.com/api/v2/"}`)
+    }))
+    defer testServer.Close()
+
+    testDomain := testServer.URL[8:]
+    result, _ := providerConfigureRaw(testServer.Client(), testDomain, clientId, clientSecret)
+    assert.Equal(Config{}, result)
+
+}
+
+func TestProviderConfigRaw(t *testing.T) {
+    assert := assert.New(t)
+
+    times := 0
+    clientSecret := "cauliflower"
+    clientId := "joebang"   
+    token := "wubbalubbadubdub"
+    
+    testServer := httptest.NewTLSServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+        times++
+        assert.Equal("POST", r.Method)
+        body, readErr := ioutil.ReadAll(r.Body)
+        if readErr != nil {
+            t.Fatalf("Failed to read request: %s", readErr)
+        }
+        
+        var clientRequest ClientRequest
+        unmarshalErr := json.Unmarshal(body, &clientRequest)
+        if unmarshalErr != nil {
+            t.Fatalf("Failed to parse request: %s", unmarshalErr)
+        }
+
+        assert.Equal(clientSecret, clientRequest.ClientSecret)
+        assert.Equal(clientId, clientRequest.ClientId)
+        assert.Equal("client_credentials", clientRequest.GrantType)
+
+        clientResponse := &Auth0Token{
+            AccessToken: token,
+            ExpiresIn: 86400,
+            Scope: "superman:all",
+            TokenType: "type",
+        }
+
+        w.WriteHeader(200)
+        json.NewEncoder(w).Encode(clientResponse)
+    }))
+    defer testServer.Close()
+
+    testDomain := testServer.URL[8:]
+    result, _ := providerConfigureRaw(testServer.Client(), testDomain, clientId, clientSecret)
+
+    assert.Equal(1, times)
+    assert.Equal(testDomain, result.(Config).domain)
+    assert.Equal(token, result.(Config).accessToken)
+}
